7b362be97e04e0aba99a0ea35215b898f3ab13bb
Dave Bonner <david_bonner@harvard.edu>
2016-03-13 12:20:36 -0400

    runs, session store fails because value too long

--- bootstrap-lti/bootstrap-lti.go
+++ bootstrap-lti/bootstrap-lti.go
@@ -1,49 +1,66 @@
 package main
 
 import (
+	"encoding/base64"
 	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 	"github.com/mrjones/oauth"
 	"github.com/penzance/bootstrap-lti-golang/lti"
 )
 
-// TODO: put these in a struct, hang launchHandler off it
 var (
 	secrets = lti.HardCodedSecretGetter{
 		"test": "secret",
 	}
 	provider     = oauth.NewProvider(secrets.SecretGetter)
 	pageTemplate = template.Must(template.New("ltiBootstrap").Parse(pageTemplateString))
+	store sessions.Store
 )
 
 func main() {
+	authenticationKey, err := base64.StdEncoding.DecodeString("aSQ9SeTlr2GDaGPUWG/3i26NswFOaNgu4UEOwBL1Z8M=")
+	if err != nil {
+		log.Fatal("unable to decode session authentication key")
+	}
+	encryptionKey, err := base64.StdEncoding.DecodeString("Z/mtqwiuzhP/7UsXisDH1+2RkGSS2NnfW5iBeDMwFyg=")
+	if err != nil {
+		log.Fatal("unable to decode session encryption key")
+	}
+	store = sessions.NewFilesystemStore("", authenticationKey, encryptionKey)
 	router := mux.NewRouter()
 	router.HandleFunc("/launch", launchHandler).Methods("POST")
-	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe("0.0.0.0:9999", nil))
+	router.HandleFunc("/launch_params", launchParamsHandler).Methods("GET")
+
+	//http.Handle("/", router)
+	ltiSessionHandler := lti.LTISessionHandler{router, *provider, store}
+	log.Fatal(http.ListenAndServe("0.0.0.0:9999", ltiSessionHandler))
 }
 
 func launchHandler(w http.ResponseWriter, r *http.Request) {
-	authorized, err := provider.IsAuthorized(r)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+	// if we get here, the middleware is happy with the launch
+	url := "/launch_params" // TODO: resolve it via router
+	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func launchParamsHandler(w http.ResponseWriter, r *http.Request) {
+	ltiLaunchParams := lti.GetLaunchParams(r)
+	if ltiLaunchParams == nil {
+		log.Println("unable to retrieve LTI params from context")
+		http.Error(w, "unable to find LTI launch params",
+			http.StatusInternalServerError)
 		return
 	}
 
-	if authorized == nil {
-		w.WriteHeader(http.StatusUnauthorized)
-	}
-
 	w.WriteHeader(http.StatusOK)
-	r.ParseForm()
-	pageTemplate.Execute(w, r.Form)
+	pageTemplate.Execute(w, ltiLaunchParams)
 }
 
-// TODO: figure out how to bundle template files with go binaries
+// TODO: figure out how to bundle template files with go binaries such that
+//       i can split this out without requiring you to run it from src dir
 const pageTemplateString = `
 <html>
   <head>
--- lti/lti.go
+++ lti/lti.go
@@ -1,10 +1,27 @@
 package lti
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
 	"github.com/mrjones/oauth"
 )
+
+const (
+	launchParamsKey string = "LTI_LAUNCH_PARAMS"
+	sessionKey string = "LTI_SESSION"
+)
+
+func init() {
+	gob.Register(&url.Values{})
+}
 
 // extend map to function as an oauth SecretGetter
 type HardCodedSecretGetter map[string]string
@@ -16,4 +33,101 @@ func (h HardCodedSecretGetter) SecretGetter(key string, header map[string]string
 	}
 	c := oauth.NewConsumer(key, secret, oauth.ServiceProvider{})
 	return c, nil
+}
+
+// TODO: authorization check methods (ie. IsAdministrator)
+// TODO: New method to supply default provider/secretgetter
+
+type LTISessionHandler struct {
+	Next http.Handler
+	Provider oauth.Provider
+	Store sessions.Store
+}
+
+func (h LTISessionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the session object
+	session, err := h.Store.Get(r, sessionKey)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// check for a new launch
+	if isLaunch(r) {
+		authorized, err := h.Provider.IsAuthorized(r)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, http.StatusText(http.StatusBadRequest),
+				http.StatusBadRequest)
+			return
+		}
+		if authorized == nil {
+			http.Error(w, http.StatusText(http.StatusUnauthorized),
+				http.StatusUnauthorized)
+			return
+		}
+
+		// TODO: better behavior if there's an existing lti session
+
+		// stick it in the session
+		r.ParseForm()
+		session.Values[launchParamsKey] = r.Form
+		if err := session.Save(r,w); err != nil {
+			log.Println("unable to save lti launch params to session:", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError)
+			return
+		}
+	}
+
+	// grab the launch params from the session, store in the context
+	val := session.Values[launchParamsKey]
+	launchParams, ok := val.(url.Values)
+	if !ok {
+		log.Println("wrong type for lti params in session")
+		http.Error(w, http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError)
+		return
+	}
+	SetLaunchParams(r, launchParams)
+
+	h.Next.ServeHTTP(w, r)
+}
+
+func isLaunch(r *http.Request) bool {
+	// save a copy of the body so we can restore it before returning
+	body, err := ioutil.ReadAll(r.Body)
+	r.Body.Close()
+	if err != nil {
+		// TODO: should we bubble this up somehow?
+		log.Println("unable to read the request body:", err)
+		return false
+	}
+	defer func(body []byte) {
+		r.Body = ioutil.NopCloser(bytes.NewReader(body))
+	}(body)
+
+	if r.Method == http.MethodPost {
+		r.Body = ioutil.NopCloser(bytes.NewReader(body))
+		if err := r.ParseForm(); err != nil {
+			// TODO: should we bubble this up somehow?
+			log.Println("unable to parse form from body:", err)
+			return false
+		}
+
+		return r.Form.Get("lti_message_type") == "basic-lti-launch-request"
+	}
+
+	return false
+}
+
+func GetLaunchParams(r *http.Request) url.Values {
+	if rv := context.Get(r, launchParamsKey); rv != nil {
+		return rv.(url.Values)
+	}
+	return nil
+}
+
+func SetLaunchParams(r *http.Request, v url.Values) {
+	context.Set(r, launchParamsKey, v)
 }
